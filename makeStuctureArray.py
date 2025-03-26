67ff88148acac9e4c66a3eaf6e614317ecc71749
priceal <priceal@emmanuel.edu>
2025-03-26 09:22:40 -0400

    2025 03 26

--- makeStuctureArray.py
+++ makeStuctureArray.py
@@ -9,30 +9,39 @@ Preprocessing of mmCIF file to extract DNA chains and store coordinate
 data in array format designed for easy use in structure analysis.
 
 """
-from Bio.PDB import MMCIFParser
 import os
 import numpy as  np
+from Bio.PDB import MMCIFParser
+from Bio.SeqUtils import seq1
 
 structureDirectory = '/home/allen/projects/DATA/db/assemblies'
 structureFile = '1qrv-assembly1.cif'
 outputDirectory = 'data'
+logFile = 'mSA.log'
 
 
-
-phosphateAtoms = { 'P', 'OP1', 'OP2' }
+'''
+###############################################################################
+########################  FUNCTIONS  ######################################
+###############################################################################
+'''
+phosphateAtoms = { 'P', 'OP1', 'OP2', 'OP3' }
 riboseAtoms = { "C1\'", "C2\'", "C3\'", "C4\'", "C5\'", "O3\'", "O4\'", "O5\'"}
 baseAtoms = { 'N1', 'C2', 'N3', 'C4', 'C5', 'C6', 'N7', 'C8', 'N9', 'N2', \
              'O6', 'N6' }
 dnaResidues = {'DA', 'DC','DG', 'DT'}
-backboneAtoms={'N','CA','O','OXT'} # for now, do not include CA (for glycine)
-
+backboneAtoms={'N','CA','O','OXT'} # for now, include CA (for glycine)
+nucleotides = "ACGT"
+aminoAcids = "ARNDCEQGHILKMFPSTWYV"
 
 def extractDnaStructure( chain ):
     
     xyzPhosphate=[]; xyzRibose=[]; xyzBase=[]; seq=[]
     for residue in chain:
-        if residue.get_resname() not in dnaResidues: continue # skip if not DNA       
-        phArray=np.ones((3,3))*np.nan # arrays to receive substructure coords
+        if residue.get_resname() not in dnaResidues: continue # skip if not DNA   
+        
+        #######################################################################
+        phArray=np.ones((4,3))*np.nan # arrays to receive substructure coords
         rbArray=np.ones((8,3))*np.nan
         baArray=np.ones((20,3))*np.nan
         ph=[]; rb=[]; ba=[]  # lists for substructure coords
@@ -48,22 +57,30 @@ def extractDnaStructure( chain ):
         phArray[:len(ph)] = np.array(ph).reshape(-1,3)
         rbArray[:len(rb)] = np.array(rb).reshape(-1,3)
         baArray[:len(ba)] = np.array(ba).reshape(-1,3)    
+        #######################################################################
         
-        # now add substructure arrays to chain lists
+        #######################################################################
+        # create the oneHot array for the sequence
+        seqArray = np.zeros(4)
+        index=nucleotides.find(residue.get_resname()[1:]) # drop the 'D'
+        seqArray[index] = 1
+        #######################################################################
+       
+        # now append substructure arrays to chain lists of residues
         xyzPhosphate.append(phArray) 
         xyzRibose.append(rbArray)
         xyzBase.append(baArray)
-        seq.append(residue.get_resname()[1:])
+        seq.append(seqArray)
         
-        
-    
+    return xyzPhosphate, xyzRibose, xyzBase, seq
+
 def extractProteinStructure( chain ):
     
     xyzBackbone=[]; xyzSidechain=[]; seq=[]
     for residue in chain:
-        if not is_aa(residue):   # skip if not aa residue
-            continue
-        
+        if not is_aa(residue): continue   # skip if not aa residue
+             
+        #######################################################################
         # arrays to receive substructure coords
         bbArray=np.ones((4,3))*np.nan; scArray=np.ones((11,3))*np.nan
         bb=[]; sc=[] # lists for substructures
@@ -73,105 +90,93 @@ def extractProteinStructure( chain ):
                 
         # fill arrays with coordinate lists
         bbArray[:len(bb)] = bb; scArray[:len(sc)] = sc
+        #######################################################################
+
+        #######################################################################
+        # create the oneHot array for the sequence
+        seqArray = np.zeros(20)
+        index=aminoacids.find(seq1(residue.get_resname())) # use 1-letter code
+        seqArray[index] = 1
+        #######################################################################
         
         # add arrays to chain lists
-        xyzBackbone.append(bbArray); xyzSidechain.append(scArray)
-        seq.append(residue.get_resname())
+        xyzBackbone.append(bbArray)
+        xyzSidechain.append(scArray)
+        seq.append(seqArray)
         
-    if xyzBackbone and xyzSidechain:   # only add to dict if an list is non-empty
-        backboneDict[chainid] = np.array(xyzBackbone)
-        sidechainDict[chainid] = np.array(xyzSidechain)
-        print('    added protein structure', end=', ')
+    return xyzBackbone, xyzSidechain, seq 
         
-    if seq:
-        sequenceDict[chainid] = seq1(''.join(seq))
-        print('    added protein sequence')
-        
-print('completed processing all chains')
+ 
+    
+    
+    
+    
 
 
-
-
-
-
-
-
-
+'''
 ###############################################################################
+########################  MAIN  ######################################
+###############################################################################
+'''
 # N.B. MMCIFParser assigned author chainids
 # the sequence determined is from the residues present in the structure
 # the seq of crystallized protein can be gotten from:
 #_entity_poly.pdbx_seq_one_letter_code_can 
 
+logOut = open(logFile,'w')
+'''
+1. load and parse the mmCIF file into a biopythn structure object
+'''
 # assume pdb id code given by first 4 characters of file name
 code = structureFile[:4]
-
 parser = MMCIFParser(QUIET=True)
 structure = parser.get_structure(code,os.path.join(structureDirectory,structureFile))
 
+# for log file
 # list number of models, and number of chains in model 0
-print(len(structure),'model(s)')
+logOut.write(f'{code}: {len(structure)} model(s). ')
 chains = []
 model = structure[0]
-for chain in model:
-    chains.append( chain.get_id() )
-print(len(model),'chain(s) in model 0:',chains)
+logOut.write(\
+   f'{len(model)} chain(s) in model 0: {tuple(model.child_dict.keys())} \n')
 
-# create the protein structure and sequence arrays
+'''
+2. setup dictionaries for protein and dna data
+'''
+# create the structure and sequence arrays
 # each entry in the dictionary will be a structure/sequence for a chain
-phosphateDict = {}; riboseDict={}; baseDict = {}
-sequenceDict = {}
+phosphateDict = {}; riboseDict={}; baseDict = {}; dnaSequenceDict = {}
+backboneDict = {}; sidechainDict={}; proteinSequenceDict = {}
 
-
-# create the protein structure and sequence arrays
-# each entry in the dictionary will be a structure/sequence for a chain
-backboneDict = {}; sidechainDict={}; sequenceDict = {}
-
-for chainid in chains:   # loop through all chains in model 0
-    print('processing structure chain', chainid)
+for chain in model:   # loop through all chains in model 0
+    logOut.write(f'processing structure chain {chain.id} ')
     
-    
-    
-    
-    if chain is dna:
+    if chain is dna:   
         
-        xyzPhosphate, xyzRibose, xyzBase, seq = \
-            extractDnaStructure( chain )
-            
-            
-    if xyzPhosphate and xyzRibose and xyzBase:   # only add if all non-empty
-        phosphateDict[chainid] = np.array(xyzPhosphate)
-        riboseDict[chainid] = np.array(xyzRibose)
-        baseDict[chainid] = np.array(xyzBase)
-
-        print('    added DNA structure', end=', ')
+        phosphate, ribose, base, seq = extractDnaStructure( chain )
+        if phosphate and ribose and base:   # only add if all non-empty
+            phosphateDict[chain.id] = np.array(phosphate)
+            riboseDict[chain.id] = np.array(ribose)
+            baseDict[chain.id] = np.array(base)
+            logOut.write('structure added. ')
         
-    if seq:
-        sequenceDict[chainid] = ''.join(seq)
-        print('    added DNA sequence')
-                
-        print('completed processing all chains')
-
-        # create the oneHot array for the sequence
-        nucleotides = "ACGT"
-        oneHotDict = {}
-        for k, v in sequenceDict.items():
-            print('chain',k)
-            pssm=np.zeros((len(v),4))
-            pssm[ range(len(v)), [nucleotides.find(c) for c in v]  ] =1
-            oneHotDict[k] = pssm
-        print('completed processing all chains')
-
-            
-            
+        if seq:
+            dnaSequenceDict[chain.id] = np.array(seq)
+            logOut.write('sequence added.\n')
+                           
     elif chain is protein:
+        logOut.write('as protein. ')
+        xyzBackbone, xyzSidechain, seq = extractProteinStructure( chain )   
+        if xyzBackbone and xyzSidechain:   # only add if all non-empty
+            backboneDict[chain.id] = np.array(xyzBackbone)
+            sidechainDict[chain.id] = np.array(xyzSidechain)
+            logOut.write('structure added. ')
+        if seq:
+            proteinSequenceDict[chain.id] = np.array(seq)
+            logOut.write('sequence added.\n')
         
-        xyzBackbone, xyzSidechain, seq=[] = \
-            extractProteinStructure( chain )   
-            
-       
-    
-    
+
+    '''
 os.makedirs(outputDirectory,exist_ok=True)
 for k in baseDict.keys():
     filePath = os.path.join(outputDirectory,code+'_'+k)
@@ -179,4 +184,5 @@ for k in baseDict.keys():
     np.savez(filePath,seq=oneHotDict[k], ph=phosphateDict[k],\
              rb=riboseDict[k], ba=baseDict[k] )
              
-             
+    '''         
+logOut.close()
